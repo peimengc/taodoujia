174965ff8cf0c0d359c7dc3cf4b9fe4bace67947
peimeng <931692760@qq.com>
2019-11-02 23:50:12 +0800

    表格自适应

--- code/resources/views/home.blade.php
+++ code/resources/views/home.blade.php
@@ -33,42 +33,47 @@
                             </div>
                         </div>
                         <div class="col-md-2">
-                            <button type="submit" class="btn btn-primary">提交</button>
-                            <button type="submit" class="btn btn-default">返回</button>
+                            <div class="btn-group">
+                                <button type="submit" class="btn btn-primary">提交</button>
+                                <button type="submit" class="btn btn-default">返回</button>
+                            </div>
                         </div>
                     </div>
                 </form>
             </div>
             <div class="col-md-12">
-                <table class="table table-hover">
-                    <thead>
-                    <tr>
-                        <th>编号</th>
-                        <th>商品</th>
-                        <th>状态</th>
-                        <th>价格</th>
-                        <th>佣金</th>
-                        <th>操作</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>
-                            <div class="checkbox">
-                                <label>
-                                    <input type="checkbox" value="">
-                                    <span>111</span>
-                                </label>
-                            </div>
-                        </td>
-                        <td>
-                        </td>
-                        <td>11</td>
-                        <td>11</td>
-                        <td>11</td>
-                    </tr>
-                    </tbody>
-                </table>
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                        <thead>
+                        <tr>
+                            <th>选中</th>
+                            <th>编号</th>
+                            <th>商品</th>
+                            <th>状态</th>
+                            <th>价格</th>
+                            <th>佣金</th>
+                            <th>操作</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" value="">
+                                    </label>
+                                </div>
+                            </td>
+                            <td>11111111111</td>
+                            <td>11111111111</td>
+                            <td>11111111111</td>
+                            <td>11111111111</td>
+                            <td>11111111111</td>
+                            <td>11111111111</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
