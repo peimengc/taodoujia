134959ca3f31deb32cb5ac5ad26514f19aedadc3
peimeng <931692760@qq.com>
2019-11-06 23:05:18 +0800

    delete null

--- code/routes/web.php
+++ code/routes/web.php
@@ -21,36 +21,6 @@ Auth::routes(['register' => false]);
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/home2', 'HomeController@index2')->name('home2');
 
-Route::get('/ordertest',function () {
-    set_time_limit(3600);
-    $orderHelper = new \App\Helpers\Tbk\TbkOrderHelper();
-    $tbkAuth = \App\Models\TbkAuthorize::query()->first();
-    if ($tbkAuth){
-       $orderHelper->getHistoryOrder($tbkAuth->access_token,'2019-11-01');
-    }
-});
-
-Route::get('/tasktest',function () {
-    set_time_limit(3600);
-    $taskHelper = new \App\Helpers\Dou\DouTopTaskGetHelper();
-    $endTime = \App\Models\DouTopTask::getNewTaskCreateTime();
-    $taskHelper->getNewTask($endTime);
-});
-
-Route::get('costtest',function () {
-    set_time_limit(3600);
-    $tasks = \App\Models\DouTopTask::getAllByCostIsNull();
-    $helper = new \App\Helpers\Dou\DouTopTaskInfoGetHelper($tasks);
-    $helper->execute();
-});
-
-Route::get('producttest',function () {
-    set_time_limit(3600);
-    $tasks = \App\Models\DouTopTask::getAwemeIdsByProductIdIsNull();
-    $helper = new \App\Helpers\Dou\DouTopTaskProductIdGetHelper($tasks);
-    $helper->execute();
-});
-
 Route::group([
     'middleware' => 'auth'
 ], function () {
