523e6ee30a844aa40251def47d80b18a7a481dcb
peimeng <931692760@qq.com>
2019-11-07 14:37:38 +0800

    时段统计

--- code/app/Http/Controllers/DouJiaTongJiController.php
+++ code/app/Http/Controllers/DouJiaTongJiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\DouTopTaskInfo;
 use App\Models\TbkOrder;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -22,29 +23,40 @@ class DouJiaTongJiController extends Controller
     public function hour()
     {
         //订单按小时分组统计
-        $orders = TbkOrder::query()
+        $orderQuery = TbkOrder::query()
             ->select(
                 DB::raw('DATE_FORMAT(tk_create_time,\'%H:00\') as hour'),
-                DB::raw('COUNT(id) as tbk_order_count'),
-                DB::raw('SUM(pub_share_pre_fee) as tbk_order_fee'),
-                DB::raw('0 as dou_task_cost')
+                DB::raw('0 as task_cost'),
+                DB::raw('COUNT(id) as order_count'),
+                DB::raw('SUM(pub_share_pre_fee) as order_fee')
             )
             ->whereDate('tk_create_time', $this->request->get('date'))
             ->where('tk_status', '!=', TbkOrder::ORDER_CLOSE_STATUS)
             ->groupBy('hour');
 
         //消耗按小时分组统计并关联订单统计
-        $data = DouTopTaskInfo::query()
+        $taskInfoQuery = DouTopTaskInfo::query()
             ->select(
                 DB::raw('DATE_FORMAT(created_at,\'%H:00\') as hour'),
-                DB::raw('SUM(cost) as dou_task_cost'),
-                DB::raw('0 as tbk_order_count'),
-                DB::raw('0 as tbk_order_fee')
+                DB::raw('SUM(cost) as task_cost'),
+                DB::raw('0 as order_count'),
+                DB::raw('0 as order_fee')
             )
             ->whereDate('created_at', $this->request->get('date'))
             ->groupBy('hour')
+            ->unionAll($orderQuery);
+
+        //投放跟订单union后再group
+        $data = DB::query()
+            ->select(
+               'hour',
+                DB::raw('SUM(task_cost) as dou_task_cost'),
+                DB::raw('SUM(order_count) as tbk_order_count'),
+                DB::raw('SUM(order_fee) as tbk_order_fee')
+            )
+            ->fromSub($taskInfoQuery,'doutask_tbkorder')
+            ->groupBy('hour')
             ->having('dou_task_cost', '>', 0)
-            ->union($orders)
             ->get();
 
         return view('douJiaTongJi.hour', compact('data'));
