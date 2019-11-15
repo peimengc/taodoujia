fe47c32c5a67192f643d1e8a1d56ceb084164480
peimeng <931692760@qq.com>
2019-11-15 18:13:35 +0800

    home data

--- code/app/Http/Controllers/HomeController.php
+++ code/app/Http/Controllers/HomeController.php
@@ -2,12 +2,61 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DouTopTaskInfo;
+use App\Models\TbkOrder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-    public function index()
+    protected $request;
+
+    public function __construct(Request $request)
     {
-        return view('home');
+        if (!$request->has('date')) {
+            $request->offsetSet('date', 1);
+        }
+        $this->request = $request;
+    }
+
+    public function index(Request $request)
+    {
+        //订单按小时分组统计
+        $order = TbkOrder::query()
+            ->select(
+                DB::raw('COUNT(id) as order_count'),
+                DB::raw('SUM(pub_share_pre_fee) as order_fee')
+            )
+            ->date($this->request->get('date'))
+            ->where('tk_status', '!=', TbkOrder::ORDER_CLOSE_STATUS)
+            ->first();
+        //消耗按小时分组统计并关联订单统计
+        $task = DouTopTaskInfo::query()
+            ->select(
+                DB::raw('SUM(cost) as task_cost')
+            )
+            ->date($this->request->get('date'))
+            ->first();
+
+        $homeMinBoxData = [
+            [
+                'name'  => '有效佣金(' . $order->order_count . ')',
+                'value' => $order->order_fee ?? 0,
+            ],
+            [
+                'name'  => 'DOU+消耗',
+                'value' => $task->task_cost ?? 0,
+            ],
+            [
+                'name'  => '淘豆荚净利',
+                'value' => ($order->order_fee - $task->task_cost) ?? 0,
+            ],
+            [
+                'name'  => '淘豆荚ROI',
+                'value' => $task->task_cost > 0 ? round($order->order_fee / $task->task_cost,2) : '--',
+            ],
+        ];
+
+        return view('home', compact('homeMinBoxData'));
     }
 }
--- code/resources/views/home.blade.php
+++ code/resources/views/home.blade.php
@@ -3,18 +3,32 @@
 @section('content')
     <div class="container py-3 home">
         <div class="row justify-content-center">
-            <div class="col-md-3 min-box">
-                <div>
-                    <h1>1</h1>
-                    <span class="c-dgray font10">111</span>
+            <div class="col-md-12 mb-3">
+                <div class="py-2 bg-white">
+                    <a href="?date=1"
+                       class="btn @if(request('date',1)==1) btn-info @else btn-secondary @endif mx-2">今日</a>
+                    <a href="?date=2"
+                       class="btn @if(request('date')==2) btn-info @else btn-secondary @endif mx-2">昨日</a>
+                    <a href="?date=3"
+                       class="btn @if(request('date')==3) btn-info @else btn-secondary @endif mx-2">七日</a>
+                    <a href="?date=4"
+                       class="btn @if(request('date')==4) btn-info @else btn-secondary @endif mx-2">本月</a>
                 </div>
             </div>
+            @foreach($homeMinBoxData as $data)
+            <div class="col-md-3 min-box">
+                <div>
+                    <h1>{{ $data['value'] }}</h1>
+                    <span class="c-dgray font10">{{ $data['name'] }}</span>
+                </div>
+            </div>
+                @endforeach
         </div>
     </div>
 @endsection
 @push('css')
     <style>
-        .home .min-box>div{
+        .home .min-box > div {
             background-color: #fff;
             text-align: center;
             padding: 1rem 0;
