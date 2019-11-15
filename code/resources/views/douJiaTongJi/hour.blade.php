cdc40e37557ae478a77fd2ce398dd4bc70cd88de
peimeng <931692760@qq.com>
2019-11-15 16:57:40 +0800

    淘豆荚统计 时段搜索

--- code/resources/views/douJiaTongJi/hour.blade.php
+++ code/resources/views/douJiaTongJi/hour.blade.php
@@ -9,6 +9,26 @@
                     <span>淘豆荚时段统计</span>
                 </div>
             </div>
+            {{--search--}}
+            <div class="col-md-12 my-3">
+                <form>
+                    <div class="row">
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                <label class="sr-only" for="search-date">日期</label>
+                                <input type="text" id="search-date" class="form-control" name="date"
+                                       value="{{request('date')}}">
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="btn-group">
+                                <button type="submit" class="btn btn-primary">提交</button>
+                                <a class="btn btn-secondary" href="{{ url()->current() }}">返回</a>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
             <div class="col-md-12">
                 <div class="table-responsive">
                     <table class="table table-hover table-data">
